e7ad3aa284e81347bef7553bd854dcee78a44bdc
奇风 <qifeng.whl@antgroup.com>
2023-08-17 09:41:25 +0800

    init

--- src/components/Nav/index.tsx
+++ src/components/Nav/index.tsx
@@ -5,7 +5,6 @@ export default ()=>{
     return <div>
         <Link href="/">Home</Link> |
         <Link href="/antd">Antd Demo</Link> |
-        <Link href="/sg">SG Demo</Link> |
         <Link href="/csr">CSR Demo</Link> |
         <Link href="/mix">Mix Demo</Link>
     </div>
