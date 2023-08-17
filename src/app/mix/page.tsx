3c590a94666b5c9889a81d100608e1fa850dff41
奇风 <qifeng.whl@antgroup.com>
2023-08-17 15:37:57 +0800

    init

--- src/app/mix/page.tsx
+++ src/app/mix/page.tsx
@@ -2,10 +2,19 @@ import React from "react";
 import Nav from "@/components/Nav";
 import CSR from "@/components/CSR";
 
-export default ()=> {
+async function getData() {
+    return new Promise<string>((resolve) => {
+        setTimeout(() => {
+            resolve('hello, nextjs')
+        }, 1000)
+    })
+}
+
+export default async () => {
+    const data = await getData();
     return <div>
         <Nav/>
-        <h1>服务端渲染内容</h1>
+        <h1>服务端渲染内容: {data}</h1>
         <CSR/>
     </div>
 }
